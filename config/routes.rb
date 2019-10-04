08af144cf8732500897c6588a9736fa8e4fb688e
suganyaS10 <sugukvs92@gmail.com>
2019-10-04 23:05:58 +0530

    Marley Spoon Coding Challenge

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'recipes#index'
 
   resources :recipes, only: [:index] do
     get :show_recipe, on: :collection
